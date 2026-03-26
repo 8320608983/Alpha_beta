a8960cdc0b45d7a1e92e73a2e553aa57c060a61c
Abhishek Pandya <162157655+8320608983@users.noreply.github.com>
2026-03-26 23:43:12 +0530

    Just Instantiate cards using manager scripts

--- Assets/Scripts/Core/GridManager.cs
+++ Assets/Scripts/Core/GridManager.cs
@@ -1,11 +1,60 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
     public static GridManager Instance;
+    public GridLayoutGroup grid;
+    public CardScript cardScript; 
 
+    public int rows;
+    public int cols;
     private void Awake()
     {
         if (Instance == null) { Instance = this; }
+    }
+    private void Start()
+    {
+        ClearGrid();
+        GenerateGrid();
+    }
+    void ClearGrid()
+    {
+        foreach (Transform child in grid.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    void GenerateGrid()
+    {
+        RectTransform rect = grid.GetComponent<RectTransform>();
+
+        float width = rect.rect.width;
+        float height = rect.rect.height;
+
+        float spacingX = grid.spacing.x;
+        float spacingY = grid.spacing.y; 
+
+        float totalSpacingX = spacingX * (cols - 1);
+        float totalSpacingY = spacingY * (rows - 1);
+
+        float cellWidth = (width - totalSpacingX) / cols;
+        float cellHeight = (height - totalSpacingY) / rows;
+
+        float size = Mathf.Min(cellWidth, cellHeight);
+        grid.cellSize = new Vector2(size, size);
+
+        grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        grid.constraintCount = cols; 
+
+        for(int i = 0; i < cols * rows; i++)
+        {
+            CardScript obj = Instantiate(cardScript, grid.transform);
+
+        }
+
+
+
     }
 }
--- Assets/Scripts/Systems/CardScript.cs
+++ Assets/Scripts/Systems/CardScript.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class CardScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] GameObject frontSide;
+    [SerializeField] GameObject backSide;
+    [SerializeField] Button btnClick;
+    private float flipTime = 0.15f;
+    internal bool isfliped;
+    private void OnEnable()
     {
-        
+        btnClick.onClick.RemoveAllListeners();
+        btnClick.onClick.AddListener(OnClick);
+    }
+    public void OnClick()
+    {
+        if (isfliped)
+            return;
+        StartCoroutine(Flip(true));
+    }
+    IEnumerator Flip(bool showFront)
+    {
+        float time = 0; 
+        while (time < flipTime)
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(1, 0, time / flipTime), 1, 1);
+            time += Time.deltaTime;
+            yield return null;
+        } 
+        frontSide.SetActive(showFront);
+        backSide.SetActive(!showFront); 
+        time = 0; 
+        while (time < flipTime)
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(0, 1, time / flipTime), 1, 1); 
+            time += Time.deltaTime;
+            yield return null;
+        } 
+        transform.localScale = Vector3.one; 
+    }
+    public void FlipBack()
+    {
+        StartCoroutine(Flip(false));
     }
 }
